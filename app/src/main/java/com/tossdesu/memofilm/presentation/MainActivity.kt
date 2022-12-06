8b1913e4893204483484d098fb07d372c15ce87f
Toss <tossdesu@gmail.com>
2022-12-06 17:56:17 +0700

    Initial commit

---
+++ app/src/main/java/com/tossdesu/memofilm/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tossdesu.memofilm.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tossdesu.memofilm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
