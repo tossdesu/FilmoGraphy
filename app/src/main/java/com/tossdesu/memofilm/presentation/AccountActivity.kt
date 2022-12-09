c83df7e79dde3923af2dcb608b3d614dd7618073
Toss <tossdesu@gmail.com>
2022-12-09 18:44:56 +0700

    Add CoordinatorLayout in activity_main layout

--- app/src/main/java/com/tossdesu/memofilm/presentation/AccountActivity.kt
+++ app/src/main/java/com/tossdesu/memofilm/presentation/AccountActivity.kt
@@ -1,7 +1,10 @@
 package com.tossdesu.memofilm.presentation
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import com.google.android.material.tabs.TabLayoutMediator
 import com.tossdesu.memofilm.R
 import com.tossdesu.memofilm.databinding.ActivityAccountBinding
